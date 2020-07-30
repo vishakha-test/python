5ede0a7882dff578115ba2964ffcbfd99f8a4d3d
diwan khan <davidpathan6@gmail.com>
2020-07-30 14:01:34 +0530

    even and odd game simple

---
+++ even_odd.py
@@ -0,0 +1,11 @@
+
+
+print('Even & Odd Game')
+while True:
+    num = int(input('enter your number: '))
+    rem = num%2
+    if rem == 0:
+        print('THIS IS EVEN')
+    else:
+        print('THIS IS ODD')
+
