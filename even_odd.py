2d92c0d8c2480fc33102fec6ada1f36d7390ebbf
vishakha-test <72201609+vishakha-test@users.noreply.github.com>
2020-10-01 17:00:53 +0530

    Update even_odd.py
    Added comments for more readability and understanding

--- even_odd.py
+++ even_odd.py
@@ -1,11 +1,11 @@
+# This programs tells you if the number entered by user is even or odd.
 
-
-print('Even & Odd Game')
-while True:
-    num = int(input('enter your number: '))
-    rem = num%2
-    if rem == 0:
+print('Even & Odd Game') # Prints the name of game at top
+while True: #This loop runs infintly 
+    num = int(input('enter your number: ')) # gets input from user and converts in into number
+    rem = num%2 # calculates the remender by using modulo operator
+    if rem == 0: # if condition to check is remender is zero and prints EVEN if true
         print('THIS IS EVEN')
-    else:
+    else: # this is excuted if the number is even
         print('THIS IS ODD')
 
